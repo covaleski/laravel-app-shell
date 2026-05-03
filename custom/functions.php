f5a5c609174bbff6945a088ac9664d59b1b7b717
covaleski <covaleski23@gmail.com>
2026-05-03 14:07:27 -0300

    feat: support class/style arrays in `attributes(...)`

--- custom/functions.php
+++ custom/functions.php
@@ -1,13 +1,21 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\View\ComponentAttributeBag;
 
 if (!function_exists('attributes')) {
     /**
      * Turn the specified array into HTML attributes.
      */
-    function attributes(array $attributes): ComponentAttributeBag
+    function attributes(array ...$attributes): ComponentAttributeBag
     {
-        return (new ComponentAttributeBag())->merge($attributes);
+        return (new ComponentAttributeBag())->merge(Arr::map(
+            array_merge_recursive(...$attributes),
+            fn ($value, $key) => match ($key) {
+                'class' => Arr::toCssClasses($value),
+                'style' => Arr::toCssStyles($value),
+                default => $value,
+            },
+        ));
     }
 }
