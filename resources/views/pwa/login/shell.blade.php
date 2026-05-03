8ae64beef4e33ea6da62912c477d493a01200a05
covaleski <covaleski23@gmail.com>
2026-05-03 20:28:31 -0300

    test: refactor blank shells

--- resources/views/pwa/login/shell.blade.php
+++ resources/views/pwa/login/shell.blade.php
@@ -1,9 +1,9 @@
-<div class="shell shell--blank" @pwaShell()>
-    <main class="page page--centered" @pwaPage()>
+<div class="shell" @pwaShell()>
+    <main @pwaPage()>
         @include($page)
     </main>
-    <footer class="footer">
-        <div class="debugbar">
+    <footer>
+        <div>
             This is the <strong>blank</strong> shell from {{ date('H:i:s') }}.
         </div>
     </footer>
