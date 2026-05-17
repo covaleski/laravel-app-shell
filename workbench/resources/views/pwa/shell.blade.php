8c8be65944cc9f651fcfdd29a7a5ac6e3ab0904e
covaleski <covaleski23@gmail.com>
2026-05-17 09:22:06 -0300

    refactor: remove the `@attributes` directive

--- workbench/resources/views/pwa/shell.blade.php
+++ workbench/resources/views/pwa/shell.blade.php
@@ -5,23 +5,22 @@
     <aside class="d-flex flex-grow-0 flex-shrink-0 bg-dark">
         <nav class="nav nav-pills nav-fill d-flex gap-2 flex-fill p-1">
             @foreach ($menus as $menu)
-                <a @attributes([
-                    'id' => $menu->home ? 'home-link' : null,
-                    'class' => [
-                        'nav-link',
-                        'link-light',
-                        'active' => $menu->active,
-                    ],
-                    'href' => $menu->url,
-                    'title' => $menu->label,
-                    'data-bs-toggle' => 'tooltip',
-                    'data-bs-placement' => 'top',
-                    'data-bs-trigger' => 'hover',
-                    'aria-label' => $menu->label,
-                    'aria-current' => $menu->active ? 'page' : null,
-                    'hx-on:click' => 'htmx.setCurrent(this, "active", "page")',
-                    'hx-replace-url' => "true"
-                ])>
+                <a  @if($menu->home)
+                    id="home-link"
+                    @endif
+                    @class(['nav-link', 'link-light', 'active' => $menu->active])
+                    href="{{ $menu->url }}"
+                    title="{{ $menu->label }}"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    data-bs-trigger="hover"
+                    @if($menu->active)
+                    aria-current="page"
+                    @endif
+                    aria-label="{{ $menu->label }}"
+                    hx-on:click="htmx.setCurrent(this, 'active', 'page')"
+                    hx-replace-url="true"
+                >
                     <i class="bi bi-{{ $menu->icon }} fs-4"></i>
                 </a>
             @endforeach
